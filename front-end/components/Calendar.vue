8eab4d3258f101a82dc64b4b52a0a6c42d827e09
jota <miki1jota@gmail.com>
2021-12-22 10:10:05 +0900

    feat: f カレンダー追加

---
+++ front-end/components/Calendar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <v-sheet tile height="6vh" color="grey lighten-3" class="d-flex align-center">
+     <v-btn outlined small class="ma-4" @click="setToday">
+       今日
+     </v-btn>
+      <v-btn icon @click="$refs.calendar.prev()">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn icon @click="$refs.calendar.next()">
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-sheet>
+    <v-sheet height="94vh">
+      <v-calendar
+        ref="calendar"
+        v-model="value"
+        :events="events"
+        :event-color="getEventColor"
+        @change="getEvents"
+        locale="ja-jp"
+        :day-format="(timestamp) => new Date(timestamp.date).getDate()"
+        :month-format="(timestamp) => (new Date(timestamp.date).getMonth() + 1) + ' /'"
+      ></v-calendar>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  data: () => ({
+    events: [],
+    value: moment().format('yyyy-MM-DD'),  // 現在日時
+  }),
+  methods: {
+    getEvents() {
+      const events = [
+        {
+          name: '会議',
+          start: new Date('2021-12-03T01:00:00'), // 開始時刻
+          end: new Date('2021-12-03T02:00:00'), // 終了時刻
+          color: 'blue',
+          timed: true, // 終日ならfalse
+        },
+      ];
+      this.events = events;
+    },
+    getEventColor(event) {
+      return event.color;
+    },
+     setToday () {
+      this.value = moment().format('yyyy-MM-DD')
+    },
+  },
+  computed: {
+    title() {
+      return moment(this.value).format('yyyy年 M月');  // 表示用文字列を返す
+   }
+ },
+};
+</script>
---
+++ front-end/pages/user/history.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Calendar :events="events"/>
+    <!-- <Calendar /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      events: [],
+    }
+  },
+}
+</script>
