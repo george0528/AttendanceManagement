f462c3165250214b46ec6ee13228e7e561f7ca06
jota <miki1jota@gmail.com>
2022-01-10 00:37:25 +0900

    feat: 給与明細を作るコマンドを作成

--- back-end/app/Console/Commands/CreatePayslip.php
+++ back-end/app/Console/Commands/CreatePayslip.php
@@ -3,9 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Models\History;
-use App\Models\Payslip;
 use App\Models\User;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class CreatePayslip extends Command
 {
@@ -38,16 +40,42 @@ class CreatePayslip extends Command
    *
    * @return int
    */
-  public function handle(Payslip $payslip)
+  public function handle()
   {
-    $users = User::with('histories')->get();
-    foreach ($users as $index => $user) {
-      $data['user_id'] = $user->id;
-      $data['attendance_days'] = count($user->histories);
-      $times = History::getTimes($user->histoies);
-      $data['sum_time'] = $times['sum_times'];
-      $data['midnight_time'] = $times['midnight_times'];
-      $payslip->create($data);
+    $users = User::with(['histories', 'salary'])->get();
+    DB::beginTransaction();
+    try {
+      $create_datas = [];
+      foreach ($users as $user) {
+        if($user->salary == null) {
+          throw new Exception($user->name."さんの給料が設定されていません");
+        }
+        // データを初期化
+        $payslip_create_data = [];
+
+        $payslip_create_data['user_id'] = $user->id;
+        $payslip_create_data['attendance_days'] = count($user->histories);
+        $time_data = History::getTimes($user->histoies);
+        $payslip_create_data['sum_time'] = $time_data['sum_times'];
+        $payslip_create_data['midnight_time'] = $time_data['midnight_times'];
+        $salary_data = $user->salary;
+        $payslip_create_data['salary_type'] = $salary_data['salary_type'];
+        $payslip_create_data['hour_salary'] = $salary_data['hour_salary'];
+        $payslip_create_data['month_salary'] = $salary_data['month_salary'];
+        $now = Carbon::now();
+        $payslip_create_data['created_at'] = $now;
+        $payslip_create_data['updated_at'] = $now;
+        
+        // 作成予定データを配列に追加
+        $create_datas[] = $payslip_create_data;
+      }
+      // 複数データをインサート
+      DB::table('payslips')->insert($create_datas);
+      DB::commit();
+    } catch (\Exception $e) {
+      DB::rollBack();
+      logger()->error($e);
+      return Command::SUCCESS;
     }
     return Command::SUCCESS;
   }
