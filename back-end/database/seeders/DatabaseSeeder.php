1537d0dd06daa565119459a071153c3e8960089f
jota <miki1jota@gmail.com>
2022-01-16 01:44:25 +0900

    update: b factoryの給料のダミーデータを変更

--- back-end/database/seeders/DatabaseSeeder.php
+++ back-end/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Salary;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,13 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(5)->create();
+        \App\Models\User::factory()->has(Salary::factory()->count(1), 'salary')->count(3)->create();
         \App\Models\Admin::factory(3)->create();
         \App\Models\History::factory(3)->create();
         \App\Models\Schedule::factory(3)->create();
         \App\Models\ShiftRequest::factory(3)->create();
         \App\Models\ShiftRequestDate::factory(3)->create();
         \App\Models\AbsenceRequest::factory(3)->create();
-        \App\Models\Salary::factory(1)->create();
     }
 }
